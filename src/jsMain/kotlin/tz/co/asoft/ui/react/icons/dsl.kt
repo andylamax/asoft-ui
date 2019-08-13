174112605d46d20037d105f85fda8931a902d9e2
Anderson Lameck <andylamax@programmer.net>
2019-08-13 17:42:47 +0300

    Include the load icon in the build

---
+++ src/jsMain/kotlin/tz/co/asoft/ui/react/icons/dsl.kt
@@ -0,0 +1,5 @@
+package tz.co.asoft.ui.react.icons
+
+import react.RBuilder
+
+fun RBuilder.loadingIcon() = loadingSvg {}
--- src/jsMain/kotlin/tz/co/asoft/ui/react/icons/loadingSvg.kt
+++ src/jsMain/kotlin/tz/co/asoft/ui/react/icons/loadingSvg.kt
@@ -1,8 +1,8 @@
+@file:JsModule("asoft-ui/loadingSvg")
 package tz.co.asoft.ui.react.icons
 
 import react.RClass
 import react.RProps
 
-@JsModule("asoft-ui/loadingSvg")
 @JsName("default")
 external val loadingSvg: RClass<RProps>
