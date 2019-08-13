700d4359f175818cb823685b6a8372d05f1a5116
Anderson Lameck <andylamax@programmer.net>
2019-08-13 15:26:15 +0300

    Added loading svg

---
+++ src/jsMain/kotlin/tz/co/asoft/ui/react/icons/loadingSvg.kt
@@ -0,0 +1,7 @@
+package tz.co.asoft.ui.react.icons
+
+import react.RClass
+import react.RProps
+import kotlinext.js.require as require
+
+val loadingSvg: RClass<RProps> = require("./leadingSvg").default
