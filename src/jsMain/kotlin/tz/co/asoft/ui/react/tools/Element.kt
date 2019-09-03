15c0bed5009874425800fcd6f8cff11c7b95c831
Anderson Lameck <andylamax@programmer.net>
2019-09-03 12:13:41 +0300

    Added Extensions on element

---
+++ src/jsMain/kotlin/tz/co/asoft/ui/react/tools/Element.kt
@@ -0,0 +1,30 @@
+package tz.co.asoft.ui.react.tools
+
+import kotlinx.css.TagSelector
+import org.w3c.dom.Element
+import org.w3c.dom.HTMLFormElement
+import org.w3c.dom.HTMLInputElement
+
+operator fun HTMLFormElement.get(name: String): String? {
+    val node = input(name)
+    return if (node == undefined) {
+        null
+    } else {
+        if (node.value == undefined) {
+            null
+        } else {
+            node.value
+        }
+    }
+}
+
+val submit get() = TagSelector("""[type="submit"]""")
+
+fun <E : Element> Element.node(selector: TagSelector) = querySelector(selector.tagName)
+fun <E : Element> Element.nodes(selector: TagSelector) = querySelectorAll(selector.tagName)
+fun <E : Element> Element.nodeNamed(name: String) = querySelector("""[name="$name"]""")
+fun Element.input(name: String) = nodeNamed<HTMLInputElement>(name)
+
+fun HTMLInputElement.type(text: String) {
+    value = text
+}
